87089216dc22d841b09783088988d00a509551d3
jadenpark4 <jpark4@hwemail.com>
2025-10-01 14:27:25 -0700

    Oct 1 Working
    Worked on Dog Part 1

---
+++ Day 1/Dog.java
@@ -0,0 +1,102 @@
+public class Dog {
+    private String name;
+    private String ownerName;
+    private int age;
+    private int dogId;
+    private char dogChar;
+    private String dogTag;
+    private boolean stillInFacility;
+
+    public Dog(String name, String ownerName, int age, int dogId) {
+        this.name = name;
+        this.ownerName = name;
+        this.age = age;
+        this.dogId = dogId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getOwnerName() {
+        return ownerName;
+    }
+
+    public void setOwnerName(String ownerName) {
+        this.ownerName = ownerName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getDogId() {
+        return dogId;
+    }
+
+    public void setDogId(int dogId) {
+        this.dogId = dogId;
+    }
+
+    public char getDogChar() {
+        return dogChar;
+    }
+
+    public void setDogChar(char dogChar) {
+        this.dogChar = dogChar;
+    }
+
+    public String getDogTag() {
+        return dogTag;
+    }
+
+    public void setDogTag(String dogTag) {
+        this.dogTag = dogTag;
+    }
+
+    public boolean isStillInFacility() {
+        return stillInFacility;
+    }
+
+    public void setStillInFacility(boolean stillInFacility) {
+        this.stillInFacility = stillInFacility;
+    }
+
+    // Spot is a good dog. They are 7 years old and belong to Sandra. They are currently in our facility. For employee use only: DogTag is 123L.
+    public String toString() {
+        if (stillInFacility) {
+            return name + " is a good dog. They are " + age + " years old and belong to " + ownerName + ". They are currently in our facility. For employee use only: DogTag is " + dogTag + ".";
+        } else {
+            return name + " is a good dog. They are " + age + " years old and belong to " + ownerName + ". They are currently NOT in our facility. For employee use only: DogTag is " + dogTag + ".";
+        }
+    }
+
+    public boolean equals(Dog other) {
+        if (this.name.equals(other.name) && this.ownerName.equals(other.ownerName) && this.age == other.age && this.dogId == other.dogId && this.dogChar == other.dogChar && this.dogTag.equals(other.dogTag) && this.stillInFacility == other.stillInFacility) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public String generateDogTag() {
+        dogTag = "" + dogId + dogChar;
+        return dogTag;
+    }
+
+    public char generateDogChar() {
+        int dig1 = dogId / 100;
+        int dig2 = (dogId % 100) / 10;
+        int dig3 = dogId % 10;
+        dogChar = (char) ('F' + ((dig1 + dig2 + dig3) % 10));
+        return dogChar;
+    }
+}
---
+++ Day 1/DogTester.java
@@ -0,0 +1,6 @@
+public class DogTester {
+    public static void main(String[] args) {
+        Dog dog1 = new Dog("Spot", "Sandra", 7, 123);
+        System.out.println(dog1.toString());
+    }
+}
