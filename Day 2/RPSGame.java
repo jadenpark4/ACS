22655f77afb04bb7ffd60456c7bf1fb845f98b7a
jadenpark4 <jpark4@hwemail.com>
2025-11-10 11:35:08 -0800

    Nov 10 Working
    Began creating the classes and started working on the methods. Plan to finish tonight or tomorrow.

---
+++ Day 2/RPSGame.java
@@ -0,0 +1,60 @@
+public class RPSGame {
+    private Player player;
+    private NPC opponent;
+
+    public RPSGame (Player player, NPC opponent) {
+        this.player = player;
+        this.opponent = opponent;
+    }
+
+    public void start() {
+
+    }
+
+    public void setPlayerValues(String name, String choice) {
+
+    }
+
+    public boolean didPlayerWin() {
+        if ((player.getChoice().equals("rock")
+        && opponent.getChoice().equals("scissors"))
+        || (player.getChoice().equals("paper")
+        && opponent.getChoice().equals("rock")) 
+        || (player.getChoice().equals("scissors")
+        && opponent.getChoice().equals("paper"))) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public String toString() {
+        return "== GAME RESULTS ==" + "\n" + player.getName() +
+            " chose " + player.getChoice() + ".\n" + "Opponent chose " +
+            opponent.getChoice() + ".\n" + 
+    }
+
+    public String displayResults() {
+        return "";
+    }
+
+    public static boolean validateChoice(String choice) {
+        if (choice.equals("rock") || choice.equals("paper")
+            || choice.equals("scissors")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static String generateRandomChoice() {
+        int num = (int) (Math.random() * 3) + 1;
+        if (num == 1) {
+            return "rock";
+        } else if (num == 2) {
+            return "paper";
+        } else {
+            return "scissors";
+        }
+    }
+}
